f27c4182085a37d450d78a48ff496ccb9d574792
Vladislav Arutiunian <108311195+VladislavArutiunian@users.noreply.github.com>
2023-06-19 14:03:18 +0300

    Feature/lapi 1 1 (#1)
    * created models and migrations
    
    * created models and migrations
    
    ---------
    
    Co-authored-by: varutyunyan <varutyunyan@team.amocrm.com>

---
+++ app/Models/Travel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Travel extends Model
+{
+    use HasFactory, Sluggable, HasUuids;
+
+    /** Specialize table @var string  */
+    protected $table = 'travels';
+
+    /** All mass assignment to all fields @var array  */
+    protected $guarded = [];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
+        );
+    }
+}
